61894dbffccd178d929db9725dc6de40bf60b8d9
Daniel Penelva <d4n.andrade@gmail.com>
2024-07-10 23:18:31 -0300

    feat: add import MatCardModule + criando formulario de login .html + customizando CSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FormsModule } from '@angular/forms';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './pages/home/home.component';
+import {MatCardModule} from '@angular/material/card';
 
 
 @NgModule({
@@ -33,7 +34,8 @@ import { HomeComponent } from './pages/home/home.component';
     HttpClientModule,
     FormsModule,
     MatSnackBarModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatCardModule
   ],
   providers: [
     provideAnimationsAsync()
