827d10c43c243114327109854c6ab949b23c2ad3
Daniel Penelva <d4n.andrade@gmail.com>
2024-07-13 23:30:27 -0300

    feat: formulario de login (parte 1) - add diretivas (ngSubmit) e [(ngModel)] + validação required + testando no console

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+  loginData = {
+    "username": '',
+    "password": ''
+  }
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+      
+  }
+
+  formSubmit(){
+    console.log('Click em botão de login', `Username: ${this.loginData.username} | senha: ${this.loginData.password}`);
+  }
 
 }
