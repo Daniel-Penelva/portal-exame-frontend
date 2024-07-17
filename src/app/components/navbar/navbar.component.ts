f5f44475835f4a5cab81af37dbf22c50ebab771b
Daniel Penelva <d4n.andrade@gmail.com>
2024-07-16 23:36:48 -0300

    feat: add método de logout na navbar feat: add visibilidade do botão de logout feat: add visibilidade do username na navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+
+  constructor(public login: LoginService){}
+
+  ngOnInit(): void { }
+
+  /* método para deslogar o usuário, removendo seus dados de autenticação e recarregando a página para refletir o estado deslogado.*/
+  public logout(){
+    this.login.logout();           // remove os dados de autenticação
+    window.location.reload();      // recarrega a página atual
+  }
 
 }
