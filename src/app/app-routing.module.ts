e32dda66ca653186d55c4c87dc75d5cb4aeb3f94
Daniel Penelva <d4n.andrade@gmail.com>
2024-07-17 23:58:07 -0300

    feat: add tag app-sidebar e router-outlet e add rota A configuração de rota com a propriedade children vai permitir definir rotas filhas ou rotas aninhadas dentro de uma rota pai. Define uma rota filha 'profile' dentro da rota 'admin'. O componente ProfileComponent será carregado quando a rota 'admin/profile' for acessada, onde o admin é a rota pai e o profile a rota filho.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,13 @@ import { DashboardComponent } from './pages/admin/dashboard/dashboard.component'
 import { UserDashboardComponent } from './pages/user/user-dashboard/user-dashboard.component';
 import { AdminGuard } from './services/admin.guard';
 import { UserGuard } from './services/user.guard';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },         // Já está definido a URL raiz para o HomeComponent que vai ser a página principal
   { path: 'signup', component: SignupComponent, pathMatch: 'full' },
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
-  {path: 'admin', component: DashboardComponent, pathMatch: 'full', canActivate:[AdminGuard]},
+  {path: 'admin', component: DashboardComponent, pathMatch: 'full', canActivate:[AdminGuard], children:[{path: 'profile', component: ProfileComponent}]},
   {path: 'user-dashboard', component: UserDashboardComponent, pathMatch: 'full', canActivate:[UserGuard]}
 ];
 
