0bd329df7229ca9a8917b6e28dfbde5573c5d319
Daniel Penelva <d4n.andrade@gmail.com>
2024-07-18 22:45:59 -0300

    feat: Obtendo informações do usuário e usando interpolação no HTML
    Utilizando o ngOnInit() para obter as informações do usuário armazenadas no LoginService quando o componente é inicializado. Aqui, obtém o usuário diretamente do armazenamento local (localStorage). E utilizando a interpolação para mostrar os valores do usuário na view

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
+
+  user: any = null;
+
+  constructor(private loginService: LoginService){}
+
+  ngOnInit(): void {
+      this.user = this.loginService.getUser();   // Aqui, obtém o usuário diretamente do armazenamento local (localStorage).
+
+// Ou posso fazer assim também, que faz um chamado de API para obter o usuário atual do servidor.
+/*    this.loginService.getCurrentUser().subscribe(
+ *      (user:any) => {
+ *        this.user = user;
+ *      }, (error) => {
+ *        alert("error");
+ *      });
+ *   
+*/
+  }
 
 }
