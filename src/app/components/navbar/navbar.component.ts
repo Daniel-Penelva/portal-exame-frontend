033ce1a42e72393dcceb2c61172d37b2287aed35
Daniel Penelva <d4n.andrade@gmail.com>
2024-07-18 23:14:31 -0300

    feat: add implementação no ngOnInit() para gerenciar o estado de login Implementando no ngOnInit() para garantir que as informações sobre o estado de login do usuário e seus detalhes estejam sempre atualizadas e reativas a mudanças. Foi utilizado o Observables e o Subjects para facilitar a comunicação entre serviços e componentes, mantendo o estado de forma centralizada e eficiente. fix: Correção de bug de typeError devido ao objeto user é null ou undefined no momento de acesso a propriedade firstname. Alterando para *ngIf = "login.isLoggedIn() && user"

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -8,9 +8,20 @@ import { LoginService } from '../../services/login.service';
 })
 export class NavbarComponent implements OnInit{
 
+  isLoggedIn = false;
+  user: any = null;
+
   constructor(public login: LoginService){}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.isLoggedIn = this.login.isLoggedIn();     // Armazena o estado de autenticação do usuário (se está logado ou não). E Chama o método do LoginService que verifica se o usuário está logado, retornando true ou false.
+    this.user = this.login.getUser();              // Armazena as informações do usuário logado. e chama o método do LoginService que obtém os detalhes do usuário a partir do armazenamento local (localStorage).
+
+    this.login.loginStatusSubject.asObservable().subscribe(data => {  // Notifica os assinantes sobre mudanças no estado de login. O asObservable converte o Subject em um Observable, permitindo que outros componentes se inscrevam para receber atualizações. E  o subscribe(...) inscreve o componente para receber notificações sempre que o estado de login mudar.
+      this.isLoggedIn = this.login.isLoggedIn();                      // Atualiza a propriedade isLoggedIn do componente com o estado de login mais recente.
+      this.user = this.login.getUser();                               // Atualiza a propriedade user do componente com os dados do usuário mais recentes.
+    })
+   }
 
   /* método para deslogar o usuário, removendo seus dados de autenticação e recarregando a página para refletir o estado deslogado.*/
   public logout(){
