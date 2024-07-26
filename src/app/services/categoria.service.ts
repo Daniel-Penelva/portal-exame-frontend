a729dbbeca167df5550545341472a5dc36f31454
Daniel Penelva <d4n.andrade@gmail.com>
2024-07-26 02:08:28 -0300

    feat: add o método listarCategoria no categoria.service No construtor recebe uma instancia de HttpClient injetada pelo Angular, essa instancia é usada para fazer as chamadas HTTP. O método listarCategoria envia uma requisição GET para obter todas as categorias disponíveis no servidor. O método retorna um Observable que representa a resposta da requisição HTTP. É importante observar que métodos que retornam Observable permitem o uso de assinaturas (subscribe) para manipular a resposta ou lidar com erros. A variável baserUrl é utilizada para centralizar a URL base do servidor, facilitando a manutenção e alteração da URL caso necessário.

--- src/app/services/categoria.service.ts
+++ src/app/services/categoria.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import baserUrl from './helper';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriaService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // método para gerar listar todas categorias - lembrando que esse método é do tipo Observable
+  public listarCategoria(){
+    return this.http.get(`${baserUrl}/categoria/`);
+  }
 }
