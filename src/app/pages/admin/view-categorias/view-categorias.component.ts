5a684b145784fe6e3cf059e7c3b687fe20ef0989
Daniel Penelva <d4n.andrade@gmail.com>
2024-07-26 02:34:58 -0300

    feat: add no componente view-categorias a exibição da lista de categorias utilizando subscribe e add o Swal para exibir mensagem de erro Esse componente ViewCategoriasComponent é responsável por exibir uma lista de categorias. Na propriedade 'categorias' é do tipo any, inicializada como um array vazio. Ela armazena a lista de categorias que serão exibidas no template. O construtor injeta uma instância de 'CategoriaService', que é utilizada para realizar operações relacionadas às categorias, como buscar todas as categorias. No método 'ngOnInit' que é executado uma vez após a inicialização do componente. Neste caso, ele está sendo usado para buscar a lista de categorias quando o componente é carregado. Utilizando o Swal: Importado de sweetalert2, é uma biblioteca que fornece alertas estilizados e modais para feedbacks de usuário. Neste caso vai ser usado para exibir uma mensagem de erro estilizada para o usuário caso a chamada HTTP falhe.

--- src/app/pages/admin/view-categorias/view-categorias.component.ts
+++ src/app/pages/admin/view-categorias/view-categorias.component.ts
@@ -1,37 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoriaService } from '../../../services/categoria.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-view-categorias',
   templateUrl: './view-categorias.component.html',
-  styleUrl: './view-categorias.component.css'
+  styleUrl: './view-categorias.component.css',
 })
-export class ViewCategoriasComponent implements OnInit{
+export class ViewCategoriasComponent implements OnInit {
+  categorias: any = [];
 
-  categorias:any = [
-    {
-      categoriaId :  1,
-      titulo: 'Liguagem de Programação',
-      descricao: 'Está é uma categoria de teste'
-    },
-    {
-      categoriaId :  1,
-      titulo: 'Liguagem de Programação',
-      descricao: 'Está é uma categoria de teste'
-    },
-    {
-      categoriaId :  1,
-      titulo: 'Liguagem de Programação',
-      descricao: 'Está é uma categoria de teste'
-    },
-    {
-      categoriaId :  1,
-      titulo: 'Liguagem de Programação',
-      descricao: 'Está é uma categoria de teste'
-    }
-  ]
+  constructor(private categoriaService: CategoriaService) {}
 
-  constructor(){}
-
-  ngOnInit(): void { }
-
+  ngOnInit(): void {
+    this.categoriaService.listarCategoria().subscribe(
+      (data: any) => {        
+        this.categorias = data;
+        console.log(this.categorias);                     // Callback executada quando a resposta é bem-sucedida. A resposta (data) é atribuída à propriedade categorias, o que atualiza a lista de categorias exibida no template.
+      },
+      (error) => {
+        console.log(error);
+        Swal.fire('Erro', 'Erro ao carregar as categorias', 'error');
+      }
+    );
+  }
 }
