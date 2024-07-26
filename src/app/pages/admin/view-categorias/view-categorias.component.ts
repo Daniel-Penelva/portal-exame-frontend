1bd28d2f16d63e5fa317eff83c1a1486fd86064d
Daniel Penelva <d4n.andrade@gmail.com>
2024-07-25 23:22:22 -0300

    feat: add array de categoria na view-categorias + add codigo html, bootstrap e css para view categoria Essa implementação vai testar o funcionamento do array das categorias, nesse array vão ter as informações do título e da descrição e como irá ficar layout para visualizar essas categorias.

--- src/app/pages/admin/view-categorias/view-categorias.component.ts
+++ src/app/pages/admin/view-categorias/view-categorias.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-view-categorias',
   templateUrl: './view-categorias.component.html',
   styleUrl: './view-categorias.component.css'
 })
-export class ViewCategoriasComponent {
+export class ViewCategoriasComponent implements OnInit{
+
+  categorias:any = [
+    {
+      categoriaId :  1,
+      titulo: 'Liguagem de Programação',
+      descricao: 'Está é uma categoria de teste'
+    },
+    {
+      categoriaId :  1,
+      titulo: 'Liguagem de Programação',
+      descricao: 'Está é uma categoria de teste'
+    },
+    {
+      categoriaId :  1,
+      titulo: 'Liguagem de Programação',
+      descricao: 'Está é uma categoria de teste'
+    },
+    {
+      categoriaId :  1,
+      titulo: 'Liguagem de Programação',
+      descricao: 'Está é uma categoria de teste'
+    }
+  ]
+
+  constructor(){}
+
+  ngOnInit(): void { }
 
 }
