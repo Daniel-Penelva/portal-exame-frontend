b95f3947dd2f983b3a44899b524da2277a196324
Daniel Penelva <d4n.andrade@gmail.com>
2024-07-30 19:59:54 -0300

    feat: add a implementação do componente view.exames para exibir uma lista de exames (utiliza subscribe) Este componente é responsável por exibir uma lista de exames ou questionários na aplicação. O Exame service vai lidar com operações relacionadas a exames (listar). A instancia 'ExameService' é injetado via injeção de dependencia, vai interagir com o backend. O Swal vai lidar com a exibição de alertas estilizados na aplicação. A propriedade exames é um array inicializado vazio, vai ser utilizado para armazenar a lista de questionários que o serviço retorna. No método ngOnInit é chamado ao ser inicializado. Dentro terá o método 'listarQuestionarios' do ExameService para obter a lista de exames. O método 'subscribe' vai lidar com resultado assíncrono. Se sucesso '(data: any)' a propriedade 'exames' é populada com os dados retornados do backend, e os dados são exibidos no console. Se houver algum erro na chamada, uma mensagem de erro é registrada no console e um alerta é exibido usando o 'Swal.fire' para notificar o usuário sobre o problema.

--- src/app/pages/admin/view-exames/view-exames.component.ts
+++ src/app/pages/admin/view-exames/view-exames.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ExameService } from '../../../services/exame.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-view-exames',
   templateUrl: './view-exames.component.html',
-  styleUrl: './view-exames.component.css'
+  styleUrl: './view-exames.component.css',
 })
-export class ViewExamesComponent {
+export class ViewExamesComponent implements OnInit {
 
+  exames: any = [
+
+  ];
+  
+  constructor(private exameService: ExameService) {}
+
+  ngOnInit(): void {
+    this.exameService.listarQuestionarios().subscribe((data: any)=>{
+      this.exames = data;
+      console.log(this.exames);
+    }, (error)=>{
+      console.log(error);
+      Swal.fire('Error', 'Erro ao carregar os questionários', 'error');
+    });
+  }
 }
