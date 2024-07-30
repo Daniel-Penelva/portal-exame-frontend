183af33044e8ee48d13cadca1d8026c064c789d3
Daniel Penelva <d4n.andrade@gmail.com>
2024-07-30 19:37:01 -0300

    feat: add o método listarQuestionarios (utiliza Observable) no exame service Este método é para listar todos os questionários disponiveis. Este método retorna o resultado da chamada this.http.get(...). Esta chamada retorna um objeto do tipo Observable, que é uma construção do Angular para trabalhar com operações assíncronas, como chamadas HTTP. O uso do HttpClient: this.http refere-se a uma instância de HttpClient, que é um serviço Angular responsável por realizar comunicações HTTP. Neste caso, ele é usado para fazer uma requisição GET para o servidor. Já a URL da requisição é construída utilizando uma base URL (baserUrl) e o caminho /exame/. A 'baseUrl' é uma constante que contém a URL base do backend da aplicação, permitindo que a parte específica do endpoint seja adicionada em cada método do serviço.

--- src/app/services/exame.service.ts
+++ src/app/services/exame.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import baserUrl from './helper';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExameService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // método para gerar listar todos os questionários - lembrando que esse método é do tipo Observable
+  public listarQuestionarios(){
+    return this.http.get(`${baserUrl}/exame/`);
+  }
 }
