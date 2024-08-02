88627f764601b9d21130c4cfcf0befedba65f883
Daniel Penelva <d4n.andrade@gmail.com>
2024-08-01 22:22:09 -0300

    feat: add o método criarQuestionario utilizando subscribe e a diretiva ngSubmit no AddExameComponent Este método criarQuestionario lida com a criação de um novo questionário (ou exame). Ele verifica se os dados fornecidos estão completos, chama um serviço para enviar os dados à API e lida com as respostas de sucesso e erro. No script, verifica a validação de titulo se está vazia ou nula e se estiver, exibe uma mensagem de erro usando o MatSnackBar e interrompe a execução do método (return). Caso contrario, chama o serviço pelo método 'criarQuestionario'  do serviço, passando os dados do questionário para criar o questionário. No tratamento de resposta de sucesso se a requisição for bem sucedida, exibe os dados retornados no console, mostra um alerta de sucesso usando Swal.fire, limpa os dados do formulário (this.exameData) para os valores iniciais, e navega através do router.navigate para a página de visualização dos exames (/admin/exames). Agora, se a requisição falhar, exibe um alerta de erro usando Swal.fire.

--- src/app/pages/admin/add-exame/add-exame.component.ts
+++ src/app/pages/admin/add-exame/add-exame.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import Swal from 'sweetalert2';
 import { CategoriaService } from '../../../services/categoria.service';
+import { ExameService } from '../../../services/exame.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-exame',
@@ -25,7 +27,7 @@ export class AddExameComponent implements OnInit{
     }
   }
   
-  constructor(private categoriaService: CategoriaService, private snack: MatSnackBar){
+  constructor(private categoriaService: CategoriaService, private snack: MatSnackBar, private exameService: ExameService, private router: Router){
 
   }
 
@@ -38,6 +40,32 @@ export class AddExameComponent implements OnInit{
       Swal.fire('error', 'Erro ao carregar os dados', 'error');
     });
   }
-  
+
+  criarQuestionario(){
+    console.log(this.exameData);
+    if(this.exameData.titulo.trim() == '' || this.exameData.titulo == null){
+      this.snack.open('Título é obrigatório', '', {duration: 3000});
+      return;
+    }
+
+    this.exameService.criarQuestionario(this.exameData).subscribe((data: any)=>{
+      console.log(data);
+      Swal.fire('Exame cadastrado', 'O exame foi cadastrado com sucesso', 'success');
+      this.exameData = {
+        titulo: '',
+        descricao: '',
+        pontosMaximos: '',
+        numeroDePerguntas: '',
+        ativo: true,
+        categoria:{
+          categoriaId: ''
+        }
+      }
+      this.router.navigate(['/admin/exames']);
+    }, (error)=>{
+      Swal.fire('Error', 'Erro ao cadastrar o exame', 'error');
+    });
+    
+  }
 
 }
