bee7f2b1d90b8127203d23815b00acd62c6f7493
Daniel Penelva <d4n.andrade@gmail.com>
2024-08-02 21:35:29 -0300

    feat: testando se a rota corresponde ao id do exame buscado (captura o id da rota).

--- src/app/pages/admin/atualizar-exame/atualizar-exame.component.ts
+++ src/app/pages/admin/atualizar-exame/atualizar-exame.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-atualizar-exame',
   templateUrl: './atualizar-exame.component.html',
-  styleUrl: './atualizar-exame.component.css'
+  styleUrl: './atualizar-exame.component.css',
 })
-export class AtualizarExameComponent {
+export class AtualizarExameComponent implements OnInit {
 
+  exameId = 0;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.exameId = this.route.snapshot.params['exameId'];
+    alert(this.exameId);
+  }
 }
