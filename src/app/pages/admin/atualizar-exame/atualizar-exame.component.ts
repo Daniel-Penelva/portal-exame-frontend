a6b70176d27b18a34f2b58590ac3369e841ad0b0
Daniel Penelva <d4n.andrade@gmail.com>
2024-08-02 22:29:18 -0300

    feat: add o carregamento dos dados de um exame especifico para edição no AtualizarExameComponent Este componente é responsável por carregar os dados de um exame específico para que ele possa ser atualizado. Ele também carrega a lista de categorias disponíveis. Vao ter três propriedades: (1) exameId = 0: Inicializa o ID do exame com 0. (2) exame: any: Variável para armazenar os dados do exame. (3) categorias: any: Variável para armazenar a lista de categorias. Dentro do método de inicialização ngOnInit obtém o ID do exame a partir dos parâmetros da rota (this.exameId = this.route.snapshot.params['exameId']), e depois chama o serviço para buscar os dados do exame e os armazena na variável exame (this.exameService.buscarQuestionarioPorId(this.exameId).subscribe(...)) e por fim chama o serviço para listar as categorias e armazena o resultado na variável categorias (this.categoriaService.listarCategoria().subscribe(...)). O objetivo do componente é usado em uma rota que inclui um parâmetro exameId. Ele carrega os dados do exame e as categorias quando o componente é inicializado, permitindo que o usuário veja e edite as informações do exame.

--- src/app/pages/admin/atualizar-exame/atualizar-exame.component.ts
+++ src/app/pages/admin/atualizar-exame/atualizar-exame.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ExameService } from '../../../services/exame.service';
+import { CategoriaService } from '../../../services/categoria.service';
 
 @Component({
   selector: 'app-atualizar-exame',
@@ -9,11 +11,27 @@ import { ActivatedRoute } from '@angular/router';
 export class AtualizarExameComponent implements OnInit {
 
   exameId = 0;
+  exame: any;
+  categorias: any;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private exameService: ExameService, private categoriaService: CategoriaService) {}
 
   ngOnInit(): void {
     this.exameId = this.route.snapshot.params['exameId'];
-    alert(this.exameId);
+    this.exameService.buscarQuestionarioPorId(this.exameId).subscribe(
+      (data) => {
+        this.exame = data;
+        console.log(this.exame);
+    }, (error) => {
+      console.log(error);
+    })
+
+    this.categoriaService.listarCategoria().subscribe(
+      (data: any) => {
+        this.categorias = data;
+        console.log(this.categorias);
+    }, (error) => {
+      alert('Erro ao carregar a categoria');
+    })
   }
 }
