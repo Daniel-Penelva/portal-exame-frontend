e5b6f2194fd4fa605abcd1bbfadb1611e4adc42f
Daniel Penelva <d4n.andrade@gmail.com>
2024-08-05 20:55:33 -0300

    feat: add o método listarPerguntasDeExame no pergunta.service.ts (utiliza Observable) Este service PerguntaService utiliza o HttpClient para fazer requisições HTTP GET ao endpoint apropriado, passando o exameId como parte da URL. O método listarPerguntasDeExame retorna um Observable, permitindo que o chamador se inscreva para receber os dados ou tratar erros quando a requisição for concluída. Este serviço é responsável por interagir com um backend para obter dados relacionados às perguntas de um exame específico. O return this.http.get(${baserUrl}/pergunta/exame/${exameId}): Faz uma requisição HTTP GET ao endpoint construído dinamicamente usando o exameId fornecido. O método retorna um Observable, que é a maneira padrão do Angular lidar com operações assíncronas, como requisições HTTP.

--- src/app/services/pergunta.service.ts
+++ src/app/services/pergunta.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import baserUrl from './helper';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PerguntaService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // método para gerar listar de todas as perguntas do exame - lembrando que esse método é do tipo Observable
+  public listarPerguntasDeExame(exameId: any){
+    return this.http.get(`${baserUrl}/pergunta/exame/${exameId}`);
+  }
 }
