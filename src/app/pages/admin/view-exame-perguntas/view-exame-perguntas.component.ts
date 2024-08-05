6cea1b9645a4f88f47351cb995e9a1566109a957
Daniel Penelva <d4n.andrade@gmail.com>
2024-08-05 20:32:30 -0300

    feat: testando a captura no console do exameId e do titulo no typescript ViewExamePerguntasComponente Declarei duas propriedades 'exameId' e 'titulo', ambas serão usadas para armazenar os parametros da rota. No construtor injeta o serviço ActivatedRoute que fornece o acesso as informações sobre a rota associada ao componente, incluindo parâmetros de rota. No momento que o componente for inicializado pelo ngOnInit é acessado os parametros da rota instantaneamente usando snapshot. O snapshot captura o estado atual da rota. Ou seja, obtém o valor do parâmetro exameId. Da mesma forma, obtém o valor do parâmetro titulo. E exibe os valores no console.

--- src/app/pages/admin/view-exame-perguntas/view-exame-perguntas.component.ts
+++ src/app/pages/admin/view-exame-perguntas/view-exame-perguntas.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-exame-perguntas',
   templateUrl: './view-exame-perguntas.component.html',
   styleUrl: './view-exame-perguntas.component.css'
 })
-export class ViewExamePerguntasComponent {
+export class ViewExamePerguntasComponent implements OnInit{
+
+  exameId: any;
+  titulo: any;
+
+  constructor(private route: ActivatedRoute){}
+
+  ngOnInit(): void {
+      this.exameId = this.route.snapshot.params['exameId'];  // Acessa os parâmetros da rota instantaneamente usando snapshot. O snapshot captura o estado atual da rota. Aqui, ele obtém o valor do parâmetro exameId.
+      this.titulo = this.route.snapshot.params['titulo'];    // Acessa os parâmetros da rota instantaneamente usando snapshot. O snapshot captura o estado atual da rota. Aqui, ele obtém o valor do parâmetro titulo.
+      console.log(this.exameId);
+      console.log(this.titulo);
+  }
 
 }
