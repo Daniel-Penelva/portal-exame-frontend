c6603b4e45b1570160761afaa42ea3c5abede43d
Daniel Penelva <d4n.andrade@gmail.com>
2024-08-05 21:08:05 -0300

    feat: add o obter da lista de perguntas do exame no typescript da ViewExamePerguntaComponent (utiliza subscribe) Este método ngOnInit inicializa as propriedades do componente com base nos parâmetros da rota e faz uma chamada a um serviço para buscar dados relacionados às perguntas de um exame. (1) Acessa os parametros da rota 'exameId' e 'titulo'. (2) Chama o serviço PerguntaService chamando o método listarPerguntasDeExame passando o this.exameId como argumento. Este método retorna um Observable. O subscribe se Se inscreve no Observable retornado pelo método listarPerguntasDeExame. A subscribe permite que  lide com os dados quando a requisição é bem-sucedida ou capture erros caso a requisição falhe. (3) Tratamento de Sucesso, se Se a requisição for bem-sucedida, a função de callback é chamada com os dados recebidos (data). Estes dados são então atribuídos à propriedade this.perguntas do componente e também são registrados no console para depuração. (4) Tratamento de erro, se a requisição falhar, a função de callback de erro é chamada com o objeto de erro (error). Este erro é então registrado no console para depuração.

--- src/app/pages/admin/view-exame-perguntas/view-exame-perguntas.component.ts
+++ src/app/pages/admin/view-exame-perguntas/view-exame-perguntas.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PerguntaService } from '../../../services/pergunta.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-view-exame-perguntas',
@@ -10,14 +12,21 @@ export class ViewExamePerguntasComponent implements OnInit{
 
   exameId: any;
   titulo: any;
+  perguntas: any = [];
 
-  constructor(private route: ActivatedRoute){}
+  constructor(private route: ActivatedRoute, private perguntaService: PerguntaService, private snack: MatSnackBar){}
 
   ngOnInit(): void {
       this.exameId = this.route.snapshot.params['exameId'];  // Acessa os parâmetros da rota instantaneamente usando snapshot. O snapshot captura o estado atual da rota. Aqui, ele obtém o valor do parâmetro exameId.
       this.titulo = this.route.snapshot.params['titulo'];    // Acessa os parâmetros da rota instantaneamente usando snapshot. O snapshot captura o estado atual da rota. Aqui, ele obtém o valor do parâmetro titulo.
-      console.log(this.exameId);
-      console.log(this.titulo);
+      
+      this.perguntaService.listarPerguntasDeExame(this.exameId).subscribe(
+        (data) => {
+          console.log(data);
+          this.perguntas = data;
+      }, (error) => {
+        console.log(error);
+      });
   }
 
 }
