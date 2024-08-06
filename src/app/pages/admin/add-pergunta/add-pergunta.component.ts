271a418052095861183243c6ab068e5a9981ee6b
Daniel Penelva <d4n.andrade@gmail.com>
2024-08-06 19:27:50 -0300

    feat: testando funcionalidade de adicionar perguntas no AddPerguntaComponent Esse componente é responsável por adicionar uma nova pergunta a um exame específico. Este componente inicializa as propriedades relacionadas ao exame e à pergunta e utiliza o Angular's ActivatedRoute para acessar parâmetros da rota. Foi declarado propriedades, (1) 'exameId' que armazena o Id do exame que será obtido da rota, (2) titulo que armazena o titulo do exame, obtido da rota, (3) pergunta que inicializa um objeto pergunta com várias propriedades, como 'exame' para armazenar informações sobre o exame, como o exameId, 'conteudo' para armazenar o conteudo da pergunta, 'opcao1', 'opcao2', 'opcao3' e 'opcao4' que armazena as quatro opções de resposta para a pergunta e a 'resposta' que armazena a resposta correta para a pergunta. No construtor Injeta o serviço ActivatedRoute, que é usado para acessar informações sobre a rota ativa, incluindo parâmetros da rota. No método ngOnInit é chamado uma vez que o componente foi inicializado. Nele obtém o parâmetro exameId da rota usando o snapshot. O snapshot captura o estado atual da rota, permitindo acesso instantâneo aos parâmetros da rota. Tb obtém o parâmetro titulo da rota usando o snapshot. Tb tem o 'this.pergunta.exame['exameId'] = this.exameId; que ' define o exameId da pergunta como o exameId obtido da rota. Isso vincula a pergunta ao exame específico.

--- src/app/pages/admin/add-pergunta/add-pergunta.component.ts
+++ src/app/pages/admin/add-pergunta/add-pergunta.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-add-pergunta',
   templateUrl: './add-pergunta.component.html',
-  styleUrl: './add-pergunta.component.css'
+  styleUrl: './add-pergunta.component.css',
 })
-export class AddPerguntaComponent {
+export class AddPerguntaComponent implements OnInit {
+  
+  exameId: any;
+  titulo: any;
+  pergunta: any = {
+    exame: {},
+    conteudo: '',
+    opcao1: '',
+    opcao2: '',
+    opcao3: '',
+    opcao4: '',
+    resposta: '',
+  };
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.exameId = this.route.snapshot.params['exameId'];
+    this.titulo = this.route.snapshot.params['titulo'];
+    this.pergunta.exame['exameId'] = this.exameId;
+  }
 }
