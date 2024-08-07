7289bf40a9ee9fba3204e5d69902a531f2d38fa4
Daniel Penelva <d4n.andrade@gmail.com>
2024-08-06 22:50:49 -0300

    feat: add o método formSubmit no componente AddPerguntaComponent (utilizando subscribe) e add a diretiva ngSubmit no AddPerguntaComponent HTML Este método é responsável por validar os campos de um formulário de criação de pergunta e enviar os dados para o servidor. O método começa verificando se cada campo obrigatório (conteudo, opcao1, opcao2, opcao3, opcao4, resposta) está preenchido. Utiliza a função trim() para remover espaços em branco no início e no final das strings e verifica se a string está vazia ou é nula. Se qualquer campo estiver vazio ou nulo, o método retorna imediatamente, interrompendo o fluxo de execução e evitando o envio do formulário. Se todas as validações forem passadas, o método chama criarPergunta do PerguntaService para enviar a pergunta ao servidor. O criarPergunta retorna um Observable, ao qual se inscreve usando subscribe. Na resposta de sucesso exibe uma mensagem de sucesso usando Swal.fire. E reseta os campos do objeto pergunta para limpar o formulário. Na resposta de error exibe uma mensagem de erro usando Swal.fire. E registra o erro no console para depuração.

--- src/app/pages/admin/add-pergunta/add-pergunta.component.ts
+++ src/app/pages/admin/add-pergunta/add-pergunta.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PerguntaService } from '../../../services/pergunta.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-add-pergunta',
@@ -7,7 +9,6 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './add-pergunta.component.css',
 })
 export class AddPerguntaComponent implements OnInit {
-  
   exameId: any;
   titulo: any;
   pergunta: any = {
@@ -20,11 +21,51 @@ export class AddPerguntaComponent implements OnInit {
     resposta: '',
   };
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private perguntaService: PerguntaService
+  ) {}
 
   ngOnInit(): void {
     this.exameId = this.route.snapshot.params['exameId'];
     this.titulo = this.route.snapshot.params['titulo'];
     this.pergunta.exame['exameId'] = this.exameId;
+  }
+
+  formSubmit() {
+    if (this.pergunta.conteudo.trim() == '' || this.pergunta.conteudo == null) {
+      return;
+    }
+    if (this.pergunta.opcao1.trim() == '' || this.pergunta.opcao1 == null) {
+      return;
+    }
+    if (this.pergunta.opcao2.trim() == '' || this.pergunta.opcao2 == null) {
+      return;
+    }
+    if (this.pergunta.opcao3.trim() == '' || this.pergunta.opcao3 == null) {
+      return;
+    }
+    if (this.pergunta.opcao4.trim() == '' || this.pergunta.opcao4 == null) {
+      return;
+    }
+    if (this.pergunta.resposta.trim() == '' || this.pergunta.resposta == null) {
+      return;
+    }
+
+    this.perguntaService.criarPergunta(this.pergunta).subscribe(
+      (data) => {
+        Swal.fire('Pergunta criada', 'A pergunta foi salva com sucesso', 'success');
+        
+        // Limpa os campos
+        this.pergunta.conteudo = '';
+        this.pergunta.opcao1 = '';
+        this.pergunta.opcao2 = '';
+        this.pergunta.opcao3 = '';
+        this.pergunta.opcao4 = '';
+        this.pergunta.resposta = '';
+    }, (error) => {
+      Swal.fire('Error', 'Erro ao salvar a pergunta na base de dados', 'error');
+      console.log(error);
+    })
   }
 }
