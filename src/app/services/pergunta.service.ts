e3ec59c68fda677b5befca92e0eec31523973088
Daniel Penelva <d4n.andrade@gmail.com>
2024-08-06 21:43:47 -0300

    feat: add o método criarPergunta no pergunta.service.ts Este método é usado para enviar uma nova pergunta ao servidor. Recebe um parametro 'pergunta' do tipo any que representa a pergunta que será enviada ao servidor para a criação. Utiliza o serviço HttpClient do Angular para fazer uma requisição HTTP POST. O URL '${baserUrl}/pergunta/' é para onde a requisição será enviada. baserUrl é uma variável que contém a URL base do seu servidor de API, e /pergunta/ é o endpoint específico para a criação de perguntas. E a 'pergunta' é o corpo da requisição, que é o objeto pergunta passado como parâmetro para o método. É do tipo Observable já que em angular o HttpClient retorna um Observable, ele trabalha com fluxos de eventos assíncronos, como requisições HTTP. Eles permitem que você assine (subscribe) para eventos que ocorrem e manipule os dados recebidos ou trate erros.

--- src/app/services/pergunta.service.ts
+++ src/app/services/pergunta.service.ts
@@ -13,4 +13,9 @@ export class PerguntaService {
   public listarPerguntasDeExame(exameId: any){
     return this.http.get(`${baserUrl}/pergunta/exame/todos/${exameId}`);
   }
+
+  // método para gerar criar pergunta do exame - lembrando que esse método é do tipo Observable
+  public criarPergunta(pergunta: any){
+    return this.http.post(`${baserUrl}/pergunta/`, pergunta);
+  }
 }
