f06733f1f3f4b97e88ed3c8ddf76601899b52d8a
Daniel Penelva <d4n.andrade@gmail.com>
2024-08-14 19:36:01 -0300

    feat: add no AtualizarPerguntaComponent o carregamento de perguntas existentes no método ngOnInit e add o método atualizarPergunta Este componente é responsável por carregar uma pergunta existente e permitir sua atualização. Primeiro foram declarados as propriedades de classe: (1) 'perguntaId' para armazena o identificador da pergunta a ser atualizada. (2) 'pergunta' para armazena os dados da pergunta que serão carregados e possivelmente editados. (3) 'exame' para armazenar informações do exame relacionado. No método ngOnInit é responsável por carregar a pergunta, ou seja,  ao inicializar o componente, ele obtém o perguntaId dos parâmetros da rota e usa o serviço PerguntaService para buscar os dados da pergunta correspondente. A pergunta é então armazenada na propriedade pergunta para edição. No método atualizarPergunta envia os dados atualizados da pergunta para o servidor usando o método atualizarPergunta do serviço PerguntaService. Se a atualização for bem-sucedida, um alerta de sucesso é mostrado usando o SweetAlert (Swal.fire), e em seguida, o usuário é redirecionado para a página que lista as perguntas associadas ao exame ao qual a pergunta pertence.

--- src/app/pages/admin/atualizar-pergunta/atualizar-pergunta.component.ts
+++ src/app/pages/admin/atualizar-pergunta/atualizar-pergunta.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PerguntaService } from '../../../services/pergunta.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-atualizar-pergunta',
   templateUrl: './atualizar-pergunta.component.html',
   styleUrl: './atualizar-pergunta.component.css'
 })
-export class AtualizarPerguntaComponent {
+export class AtualizarPerguntaComponent implements OnInit{
+
+  perguntaId: any = 0;
+  pergunta: any;
+  exame: any;
+
+  constructor(private route: ActivatedRoute, private perguntaService: PerguntaService, private router: Router){}
+
+  ngOnInit(): void {
+    this.perguntaId = this.route.snapshot.params['perguntaId'];
+    this.perguntaService.obterPerguntaPorId(this.perguntaId).subscribe(
+      (data: any) => {
+        this.pergunta = data;
+        console.log(this.pergunta);
+    }, (error) => {
+      console.log(error);
+    });
+  }
+
+  atualizarPergunta(){
+    this.perguntaService.atualizarPergunta(this.pergunta).subscribe(
+      (data: any) => {
+        Swal.fire('Pergunta Atualizada', 'Pergunta atualizada com sucesso', 'success').then(
+          (e) => {
+            this.router.navigate(['/admin/ver-perguntas/'+this.pergunta.exame.exameId+'/'+this.pergunta.exame.titulo]);
+        })
+    })
+  }
 
 }
