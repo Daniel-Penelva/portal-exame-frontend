df01655878632690853d3aebecc2e2bdbd0297cc
Daniel Penelva <d4n.andrade@gmail.com>
2024-08-16 21:16:01 -0300

    feat: add no componente SidebarUser (.ts) o carregamento e a exibição da lista de categorias Este componente é responsável por carregar e exibir uma lista de categorias na barra lateral de um aplicativo. Definido a propriedade 'categorias'  armazenará a lista de categorias recebidas do serviço. Ela é do tipo any, o que significa que pode conter qualquer tipo de dado, mas neste caso, será uma lista de categorias. No construtor injeta duas dependencias (1) do categoriaService que é um serviço que fornece métodos para interagir com a API relacionada a categorias e (2) MatSnackBar que é um serviço do Angular Material que exibe mensagens temporárias na tela, útil para notificações e alertas. No método ngOnInit que é o primeiro método a ser inicializdo pelo componente. Nele tem o método listarCategoria() do CategoriaService é chamado para buscar a lista de categorias do servidor. O subscribe que é é utilizado para lidar com a resposta da chamada assíncrona. Se a chamada for bem-sucedida ((data) => { this.categorias = data; }), a resposta (lista de categorias) é armazenada na propriedade categorias. Senão se ocorrer um erro durante a chamada, uma mensagem de erro é exibida usando MatSnackBar, e o erro é registrado no console.

--- src/app/pages/user/sidebar-user/sidebar-user.component.ts
+++ src/app/pages/user/sidebar-user/sidebar-user.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoriaService } from '../../../services/categoria.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-sidebar-user',
   templateUrl: './sidebar-user.component.html',
   styleUrl: './sidebar-user.component.css'
 })
-export class SidebarUserComponent {
+export class SidebarUserComponent implements OnInit{
 
+  categorias: any;
+
+  constructor(private categoriaService: CategoriaService, private snack: MatSnackBar){}
+
+  ngOnInit(): void {
+    this.categoriaService.listarCategoria().subscribe(
+      (data) => {
+        this.categorias = data;
+    }, (error) => {
+      this.snack.open('Erro ao carregar as categorias', '', { duration: 3000 });
+      console.log(error);
+    })
+  }
 }
