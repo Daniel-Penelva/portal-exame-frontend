ee65248f3e09d8b41158d26bf6ba5eca8b3e0c21
Daniel Penelva <d4n.andrade@gmail.com>
2024-08-19 21:11:23 -0300

    feat: alterando o método ngOnInit no componente LoadExameComponent Foi alterado para carregar e exibir exames com base no parâmetro catId obtido da rota. Foi add no construtor ExameService para ser injetado os métodos que irá interagir com a API relacionada aos exames. O this.catId = this.route.snapshot.params['catId'];: Aqui, o 'catId' é obtido dos parâmetros da URL no momento da inicialização do componente. Se o catId for igual a zero indica que todos os exames devem ser carregados. No this.exameService.listarQuestionarios().subscribe(...): Aqui, o método listarQuestionarios() do ExameService é chamado, que retorna um Observable contendo todos os exames. Quando a resposta é recebida com sucesso, a lista de exames é armazenada na propriedade exames, e os dados são exibidos no console ((data) => { this.exames = data; console.log(this.exames); }).

--- src/app/pages/user/load-exame/load-exame.component.ts
+++ src/app/pages/user/load-exame/load-exame.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ExameService } from '../../../services/exame.service';
 
 @Component({
   selector: 'app-load-exame',
@@ -9,13 +10,21 @@ import { ActivatedRoute } from '@angular/router';
 export class LoadExameComponent implements OnInit {
   
   catId: any;
+  exames: any;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private exameService: ExameService) {}
 
   ngOnInit(): void {
     this.catId = this.route.snapshot.params['catId'];
     if (this.catId == 0) {
       console.log('Carregando todos os exames');
+      this.exameService.listarQuestionarios().subscribe(
+        (data) => {
+          this.exames = data;
+          console.log(this.exames);
+      }, (error) => {
+        console.log(error);
+      })
     } else {
       console.log('Carregando um exame em específico');
     }
