a47153a815342ca977d9ef4d1b213a4a141daeec
Daniel Penelva <d4n.andrade@gmail.com>
2024-08-20 21:06:52 -0300

    feat: add no método ngOnInit a busca e exibição das informações de um exame especifico no InstrucaoComponent Neste componente InstrucaoComponent tem como objetivo buscar e exibir as informações de um exame específico, utilizando seu ID. (1) Propriedades do Componente: 'exameId' que armazena o id do exame que será buscado e o 'exame' que é um objeto que armazena os detalhes do exame recuperado do backend. Inicialmente, é definido como um novo objeto vazio. (2) Construtor: São injetados duas dependencias 'ExameService' utilizado para fazer chamadas HTTP e buscar as informações do exame e 'ActivatedRoute' que permite acessar os parâmetros da rota, especificamente o exameId. (3) Método ngOnInit: Este método é executado quando o componente é inicializado. Já o this.exameId = this.route.snapshot.params['exameId'] obtém o parâmetro exameId da URL, que é utilizado para identificar o exame que será buscado. Já o this.exameService.buscarQuestionarioPorId(this.exameId).subscribe(...) faz uma chamada ao serviço ExameService para buscar os detalhes do exame usando o ID obtido. A resposta é então atribuída à propriedade exame. Já o console.log(this.exame) imprime no console os detalhes do exame obtido, útil para depuração.

--- src/app/pages/user/instrucao/instrucao.component.ts
+++ src/app/pages/user/instrucao/instrucao.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ExameService } from '../../../services/exame.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-instrucao',
   templateUrl: './instrucao.component.html',
   styleUrl: './instrucao.component.css'
 })
-export class InstrucaoComponent {
+export class InstrucaoComponent implements OnInit{
+
+  exameId: any;
+  exame: any = new Object();
+
+  constructor(private exameService: ExameService, private route: ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.exameId = this.route.snapshot.params['exameId'];
+    this.exameService.buscarQuestionarioPorId(this.exameId).subscribe(
+      (data) => {
+        this.exame = data;
+        console.log(this.exame);
+    }, (error) => {
+      console.log(error);
+    });
+  }
 
 }
