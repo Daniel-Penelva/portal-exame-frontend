1d60a07e5d07d5c65593b3910ff340dba6bb42d1
Daniel Penelva <d4n.andrade@gmail.com>
2024-08-22 20:48:30 -0300

    feat: add o método iniciarExame no InstrucaoComponent (.ts) e add o event binding (click) no InstrucaoComponent (.html) Este método exibe uma mensagem de confirmação usando a biblioteca SweetAlert2 (Swal). O objetivo é perguntar ao usuário se ele deseja realmente iniciar um exame, e dependendo da resposta, redirecioná-lo para a página de início do exame. (1) Swal.fire: Exibe um pop-up de alerta ao usuário com uma mensagem, a mensagem pergunta se o usuário quer começar o exame e inclui botões para confirmar (Começar) ou cancelar a ação. (2) then((result: any) => { ... }): Após o usuário interagir com o pop-up, o método then() captura o resultado da interação. O "result.isConfirmed" verifica se o usuário clicou no botão de confirmação ("Começar"). (3) Redirecionamento: Se o usuário confirmar que quer iniciar o exame (result.isConfirmed é true), o método usa o Router do Angular (this.router.navigate) para redirecioná-lo para a rota de início do exame. A rota de destino é /start/:exameId, onde exameId é o identificador do exame que foi definido anteriormente no componente.

--- src/app/pages/user/instrucao/instrucao.component.ts
+++ src/app/pages/user/instrucao/instrucao.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ExameService } from '../../../services/exame.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-instrucao',
@@ -12,7 +13,7 @@ export class InstrucaoComponent implements OnInit{
   exameId: any;
   exame: any = new Object();
 
-  constructor(private exameService: ExameService, private route: ActivatedRoute){}
+  constructor(private exameService: ExameService, private route: ActivatedRoute, private router: Router){}
 
   ngOnInit(): void {
     this.exameId = this.route.snapshot.params['exameId'];
@@ -23,6 +24,20 @@ export class InstrucaoComponent implements OnInit{
     }, (error) => {
       console.log(error);
     });
+  }
+
+  iniciarExame(){
+    Swal.fire({
+      title: 'Quer começar o exame?',
+      showCancelButton: true,
+      cancelButtonText: 'Cancelar',
+      confirmButtonText: 'Começar',
+      icon: 'info'
+    }).then((result: any) => {
+      if(result.isConfirmed){
+        this.router.navigate(['/start/'+this.exameId]);
+      }
+    })
   }
 
 }
