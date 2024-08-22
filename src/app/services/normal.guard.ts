5f0ec7e86a34a6cb6d832aca4aa3d8545e8f3f35
Daniel Penelva <d4n.andrade@gmail.com>
2024-08-22 20:12:27 -0300

    feat: add a definição de uma guarda de rota para o papel normal Este guard define uma guarda de rota (NormalGuard) no aplicativo Angular, que é usada para restringir o acesso a determinadas rotas com base nas permissões de usuário. (1) Decorator @Injectable: Faz com que o serviço seja injetado em toda a aplicação (singleton), garantindo que exista apenas uma instância do serviço durante o ciclo de vida da aplicação. (2) Classe NormalGuard: A classe NormalGuard implementa a interface CanActivate, que define a lógica necessária para determinar se uma rota pode ser ativada ou não. (3) Construtor: Injeta dois serviços essenciais, o 'LoginService' que é utilizado para verificar o status de login e o papel do usuário e o "Router" que é usado para redirecionar o usuário caso ele não tenha permissão para acessar a rota. (4) Método canActivate: Este método é o núcleo da guarda e determina se o usuário pode ou não ativar uma rota específica. Nos parâmetros o "route: ActivatedRouteSnapshot" contém as informações sobre a rota que está sendo acessada no momento e o "state: RouterStateSnapshot" contém o estado atual do roteador. Já na lógica verifica se o usuário está logado (this.loginService.isLoggedIn()) e se o papel do usuário é "NORMAL" (this.loginService.getUserRole() == 'NORMAL'). Se as duas condições forem verdadeiras, a rota é ativada retornando true. Se qualquer uma das condições falhar, o usuário é redirecionado para a página de login (this.router.navigate(['login'])) e a rota não é ativada, retornando false.

--- src/app/services/normal.guard.ts
+++ src/app/services/normal.guard.ts
@@ -1,5 +1,27 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { LoginService } from "./login.service";
+import { Observable } from "rxjs";
 
-export const normalGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NormalGuard implements CanActivate {
+
+  constructor(private loginService: LoginService, private router: Router){
+
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if(this.loginService.isLoggedIn() && this.loginService.getUserRole() == 'NORMAL'){
+        return true;
+      }
+
+      this.router.navigate(['login']);
+      return false;
+  }
+
+}
