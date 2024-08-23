5c660a6bc72d5938064aaec3190cbcb5550628f2
Daniel Penelva <d4n.andrade@gmail.com>
2024-08-22 21:18:32 -0300

    feat: add o método desativarBotaoVoltar no StartComponent (.ts) utilizando pushState Está funcionaldiade específica para desativar o botão "Voltar" do navegador durante a realização de um exame, impedindo que o usuário retorne à página anterior. (1) LocationStrategy: é  uma classe do Angular que lida com a estratégia de navegação (localização) usada na aplicação. Vai ser injetada no construtor para ser utilizada no método desativarBotaoVoltar. (2) método ngOnInit: Este método é chamado logo após a criação do componente. Nele, o método desativarBotaoVoltar() é invocado. (3) método desativarBotaoVoltar: No "history.pushState(null, null!, location.href);" O método pushState do objeto history permite manipular o histórico de navegação. Aqui, ele é chamado para adicionar uma nova entrada no histórico com o mesmo URL atual, mas sem navegar para outra página. Já o "this.locationSt.onPopState(...)" o onPopState é um evento que é acionado quando o usuário tenta navegar para a página anterior usando o botão "Voltar" do navegador. Dentro do callback, "history.pushState(null, null!, location.href);" é chamado novamente, o que efetivamente "anula" a ação de voltar ao estado anterior, mantendo o usuário na mesma página.
    O proposito é prevenir o retrocesso, ou seja, a principal finalidade desse método é impedir que o usuário use o botão "Voltar" do navegador para sair da página do exame. Isso é útil em situações onde permitir o retrocesso pode comprometer a integridade do exame, como em aplicações de testes online, onde sair da página pode resultar em perda de progresso ou trapaças.
    pesquisa: https://imasters.com.br/front-end/pushstate-em-10-minutos

--- src/app/pages/user/start/start.component.ts
+++ src/app/pages/user/start/start.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { LocationStrategy } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-start',
   templateUrl: './start.component.html',
   styleUrl: './start.component.css'
 })
-export class StartComponent {
+export class StartComponent implements OnInit{
+
+  constructor(private locationSt: LocationStrategy){}
+
+  ngOnInit(): void {
+      this.desativarBotaoVoltar();
+  }
+
+  desativarBotaoVoltar(){
+    history.pushState(null, null!, location.href);
+    this.locationSt.onPopState(
+      () => {
+        history.pushState(null, null!, location.href);
+    })
+  }
 
 }
