d1c038bb442eb17da34e165d5d06dfc43219b0bb
Daniel Penelva <d4n.andrade@gmail.com>
2024-08-23 20:44:09 -0300

    feat: add o método carregarPerguntas no StartComponent (.ts) (1) Propriedades: "exameId" que armazena o identificador do exame atual, extraído da rota e "perguntas" que armazena as perguntas do exame que serão carregadas e exibidas para o usuário. (2) Construtor: Injetam as dependencias ActivatedRoute que é utilizado para acessar os parâmetros da rota ativa, como o exameId e "PerguntaService" que é o serviço responsável por buscar as perguntas do exame via API. (3) No método ngOnInit: Este método é executado quando o componente é inicializado. O "this.exameId = this.route.snapshot.params['exameId'];" extrai o exameId dos parâmetros da rota. Isso permite que o componente saiba qual exame está sendo iniciado. O "this.carregarPerguntas();" chama o método que faz a requisição para carregar as perguntas do exame. (4) método carregarPerguntas: Este método faz uma requisição ao serviço PerguntaService para listar todas as perguntas associadas ao exame. O "this.perguntaService.listarPerguntaDoExameParaProva(this.exameId).subscribe(...)" chama o método listarPerguntaDoExameParaProva no serviço PerguntaService, passando o exameId para buscar as perguntas correspondentes. O "(data: any) => { ... }" se a requisição for bem-sucedida, as perguntas são armazenadas na propriedade perguntas e também são logadas no console. O "(error) => { ... }" em caso de erro, uma mensagem de alerta é exibida usando a biblioteca Swal.fire, informando ao usuário que ocorreu um erro ao carregar as perguntas. Execução - Quando o componente é carregado: (1) - O exameId é extraído da URL. (2) - As perguntas do exame correspondente são carregadas e exibidas. (3) - Se algo der errado durante o carregamento das perguntas, o usuário será notificado com um alerta de erro.

--- src/app/pages/user/start/start.component.ts
+++ src/app/pages/user/start/start.component.ts
@@ -1,5 +1,8 @@
 import { LocationStrategy } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PerguntaService } from '../../../services/pergunta.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-start',
@@ -8,12 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartComponent implements OnInit{
 
-  constructor(private locationSt: LocationStrategy){}
+  exameId: any;
+  perguntas: any;
+
+  constructor(private locationSt: LocationStrategy, private route: ActivatedRoute, private perguntaService: PerguntaService){}
 
   ngOnInit(): void {
       this.desativarBotaoVoltar();
+      
+      this.exameId = this.route.snapshot.params['exameId'];
+      console.log(this.exameId);
+      this.carregarPerguntas();
   }
 
+  // Este método carrega as perguntas relacionadas ao exame específico que está sendo iniciado.
+  carregarPerguntas(){
+    this.perguntaService.listarPerguntaDoExameParaProva(this.exameId).subscribe(
+      (data: any) => {
+        console.log(data);
+        this.perguntas = data;
+    }, (error) => {
+      Swal.fire('Error', 'Erro ao carregar as perguntas da prova', 'error');
+    });
+  }
+
+
+  // Este método desativa o botão "Voltar" do navegador durante a realização de um exame, impedindo que o usuário retorne à página anterior.
   desativarBotaoVoltar(){
     history.pushState(null, null!, location.href);
     this.locationSt.onPopState(
