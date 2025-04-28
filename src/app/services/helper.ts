08563dbb1f21c2e0c2a9c2d1d1a5d6c07af92598
Daniel Penelva <d4n.andrade@gmail.com>
2025-04-28 20:34:27 -0300

    feat: (funcionalidade: deploy) Alterando a url no helper.ts e no envronments.ts e environments.prod.ts

--- src/app/services/helper.ts
+++ src/app/services/helper.ts
@@ -1,2 +1,3 @@
-const baserUrl = 'http://localhost:8080';
+//const baserUrl = 'http://localhost:8080';*/
+const baserUrl = 'https://portal-exame-backend-production.up.railway.app';
 export default baserUrl;
